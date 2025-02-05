a4c32ec4fb309b209c75792793b5f12aed222fc8
henrique-serra <66788126+henrique-serra@users.noreply.github.com>
2025-02-05 14:35:20 +0000

    Finish layout

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const myLibrary = [];
 
+const dialog = document.querySelector("dialog");
+const addBookBtn = document.querySelector(".add-book-btn");
+const closeButtons = document.querySelectorAll(".close-modal");
+
+// "Show the dialog" button opens the dialog modally
+addBookBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+// "Close" button closes the dialog
+closeButtons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        dialog.close();
+      });
+});
+
 function Book(){
 
 };
